0e51c8acd4cd6d4e5f7a1c88ef3b7b98122b272f
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2024-04-22 20:44:44 +0200

    Class 12

---
+++ g7/Class10/Class10/Disposable/OurReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposable
+{
+    public class OurReader : IDisposable
+    {
+        private string path;
+        private StreamWriter _sr;
+        private bool disposedValue = false;
+
+        public OurReader(string filePath)
+        {
+            path = filePath;
+            _sr = new StreamWriter(filePath);
+        }
+        public string Read()
+        {
+            return _sr.ReadToEnd();
+
+
+        }
+        private void _dispose(bool disposing) 
+        {
+            if(!disposedValue)
+            {
+                if(disposing)
+                {
+                    _sr.Dispose();
+                }
+                path = "";
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            _dispose(true);
+        }
+    }
+}
---
+++ g7/Class10/Class10/Disposable/OurWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposable
+{
+    public class OurWriter : IDisposable
+    {
+        private string path;
+        private StreamWriter _sw;
+        private bool disposedValue = false;
+
+        public OurWriter(string filePath)
+        {
+            path = filePath;
+            _sw = new StreamWriter(path, true);
+        }
+
+        public void Write(string text)
+        {
+            if (text == "break")
+                throw new Exception
+                    ("Something broke unexpectedly");
+            _sw.WriteLine(text);
+        }
+        // We implement private method that will
+        // remeber when class is disposed
+        // If the same class tries disposing again
+        //the Dispose() method will not be called again
+        private void _dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _sw.Dispose();
+                }
+                path = "";
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            _dispose(true);
+        }
+    }
+}
